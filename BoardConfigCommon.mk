e703f29eecc7f78ccee8297d57586c8328aea4f0
onettboots <blackcocopet@gmail.com>
2024-10-29 16:40:53 +0000

    sm8250-common: kernel: Update flags for clang 20+
    clang: error: version 'kernel' in target triple 'arm-unknown-linux-androidkernel' is invalid

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,6 +102,10 @@ TARGET_KERNEL_CONFIG := \
     vendor/debugfs.config \
     vendor/xiaomi/${TARGET_DEVICE}.config
 
+# Kernel Clang Flags
+KERNEL_CC := CC=clang
+override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
+
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
